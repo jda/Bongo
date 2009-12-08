22bbb7a82344e6679f2bba2357960fe4ff1a58a9
Jon Auer <jauer@netwurx.net>
2009-12-07 23:09:36 -0600

    basic feel is here, with style

--- src/Bongo/Web/Admin.pm
+++ src/Bongo/Web/Admin.pm
@@ -7,30 +7,62 @@ use strict;
 package Bongo::Web::Admin;
 use base 'CGI::Application';
 
+use CGI::Application::Plugin::AutoRunmode;
+use CGI::Application::Plugin::ValidateRM;
+use CGI::Application::Plugin::AnyTemplate;
+use HTML::Template;
+
 use Data::Dumper;
 
-sub setup {
+
+sub cgiapp_prerun {
   my $self = shift;
-  $self->start_mode('list');
-  $self->error_mode('errMode');
-  $self->run_modes(
-    'list' => 'listAPs',
-    'add' => 'addForm',
+  
+  # not sure if template path is relative to module or cgi runner dir
+  my @tPaths = qw(../../../tmpl ../tmpl);
+  
+  $self->template->config(
+    default_type => "HTMLTemplate",
+    include_paths => \@tPaths,
   );
 }
 
-sub listAPs {
-  return Dumper(@INC); 
+sub listAPs : StartRunmode {
+  my $self = shift;
+
+  my %params = (
+    title => 'AP List',
+  );
+  $self->template->fill(\%params);
 }
 
-sub addAPform {
+sub addAP : Runmode {
+  my $self = shift;
+  my $form = 1;
 
-};
+  my %params = (
+    title => 'Add AP',
+    form => $form,
+  );
+  $self->template->fill(\%params);
+}
 
-sub errMode {
-  my ($self, $err) = @_;
-  my $display = "Error encountered: $err'; stopped";
-  return $display;
+sub modAP : Runmode {
+  my $self = shift;
+
+  my %params = (
+    title => 'Modify AP',
+  );
+  $self->template->fill(\%params);
+}
+
+sub delAP : Runmode {
+  my $self = shift;
+
+  my %params = (
+    title => 'Remove AP',
+  );
+  $self->template->fill(\%params);
 }
 
 1;
